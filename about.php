6f9a2b15584c349ebee49713e33db70b588efa8a
John Paul Bayoneto <bayonetojohnpaul@gmail.com>
2023-10-30 18:10:25 +0800

    Initial commit

---
+++ about.php
@@ -0,0 +1,125 @@
+<?php 
+    include("header.html");
+    include("spinner.html");
+    include("navbar.html");
+?>
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container">
+            <h1 class="display-3 mb-3 animated slideInDown">About Us</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb mb-0">
+                    <li class="breadcrumb-item"><a class="text-body" href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a class="text-body" href="#">Pages</a></li>
+                    <li class="breadcrumb-item text-dark active" aria-current="page">About Us</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
+
+    <!-- About Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="about-img position-relative overflow-hidden p-5 pe-0">
+                        <img class="img-fluid w-100" src="img/about-pic.jpeg" style="height: 500px; width: 450px;">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                    <h1 class="display-5 mb-4">PROGREEN WEED CONTROL SOLUTIONS</h1>
+                    <p class="mb-4">Progreen supply a wide range of weed killers, moss and algae control products, fertilisers and grass seed.  We also specialise in providing a comprehensive range of sprayers, including Cooper Pegler knapsack sprayers, granule application equipment and accessories.  In terms of education and training, we offer both conventional classroom based training on the safe use of pesticides and e-learning courses in many of the sectors listed above.
+
+                        We pride ourselves in offering “trusted expertise” where our customers receive the optimum product or service for their needs at the best possible price. This is why we regularly monitor our prices to ensure you get a fair and square price. All our expert advice is provided free of charge from our team of professionally qualified FACTS & BASIS advisers.
+                        
+                        We hope you find what you’re looking from our website, but if you can’t find a particular product we can often source it. If you are not sure what you want you can always call us on 01778 394052 or email us and we’ll be more than happy to help.</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Wide Range of Products</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Competitive Pricing</p>
+                    <p><i class="fa fa-check text-primary me-3"></i>Customer Support</p>
+                    <a class="btn btn-primary rounded-pill py-3 px-5 mt-3" href="">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+
+
+    <!-- Feature Start -->
+    <div class="container-fluid bg-light bg-icon my-5 py-6">
+        <div class="container">
+            <div class="section-header text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <h1 class="display-5 mb-3">Pillars</h1>
+                <br>
+            </div>
+            <div class="row g-4">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="bg-white text-center h-100 p-4 p-xl-5">
+                        <img class="img-fluid mb-4" src="img/icon_PP_Managing_Forests.png" alt="">
+                        <h4 class="mb-3">Pillar 1</h4>
+                        <p class="mb-4">Strengthening Forest and Landscape Management</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Read More</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="bg-white text-center h-100 p-4 p-xl-5">
+                        <img class="img-fluid mb-4" src="img/icon_PP_Reducing_Agriculture.png" alt="">
+                        <h4 class="mb-3">Pillar 2</h4>
+                        <p class="mb-4">Promoting Sustainable Landscape Practices</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Read More</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="bg-white text-center h-100 p-4 p-xl-5">
+                        <img class="img-fluid mb-4" src="img/icon_PP_Supporting_Industry.png" alt="">
+                        <h4 class="mb-3">Pillar 3</h4>
+                        <p class="mb-4">Supporting Cross-sectoral Landscape Solutions</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Feature End -->
+
+     <!-- Firm Visit Start -->
+     <div class="container-fluid bg-primary bg-icon mt-0 py-6 green" style="max-width: 100%; height: 100%;">
+        <div class="container-xl">
+        <div class="section-header text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <h1 class="display-5 mb-3 text-white">Cross-Cutting Themes</h1>
+                <br>
+            </div>
+        <div class="row g-4">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="green text-center h-100 p-4 p-xl-5 text-white">
+                        <img class="img-fluid mb-4" src="img/icon_CC_Woman_Youth_0.png" alt="">
+                        <p class="mb-4">Engaging Communities and Vulnerable Groups</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill lime-green" href="">Read More</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="green text-center h-100 p-4 p-xl-5 text-white">
+                        <img class="img-fluid mb-4" src="img/icon_CC_Climate_Change_1.png" alt="">
+                        <p class="mb-4">Nature–Based Solutions: Climate Change Mitigation & Adaptation</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill lime-green" href="">Read More</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="green text-center h-100 p-4 p-xl-5 text-white">
+                        <img class="img-fluid mb-4" src="img/icon_CC_Maximizing_Finance_0.png" alt="">
+                        <p class="mb-4">Maximizing Finance for Development</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill lime-green" href="">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Firm Visit End -->
+
+
+    <!-- Footer Start -->
+    <?php 
+        include("footer.html");
+    ?>
+    <!-- Footer End -->
