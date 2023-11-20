0894b0fb34cc44a13f25fba85573c319d40e247b
XeroXCopY5373 <93362420+XeroXCopY5373@users.noreply.github.com>
2023-11-20 20:25:10 +0800

    Made The Attendance of Employee connected to Database

--- employee/profile-detail.php
+++ employee/profile-detail.php
@@ -112,10 +112,6 @@ if ($row = $result->fetch_assoc()) {
 							<ul>
 								<li><a href="profile.php">Employement</a></li>
 								<li><a class="active" href="#">Detail</a></li>
-								<li><a href="profile-document.php">Document</a></li>
-								<li><a href="profile-payroll.php">Payroll</a></li>
-								<li><a href="profile-timeoff.php">Timeoff</a></li>
-								<li><a href="profile-review.php">Reviews</a></li>
 								<li><a href="profile-setting.php">Settings</a></li>
 							</ul>
 						</div>
