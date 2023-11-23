71a28434b7815a726789316d92257998873e0f57
John Paul Bayoneto <bayonetojohnpaul@gmail.com>
2023-11-24 01:24:06 +0800

    remove icons

--- feature.php
+++ feature.php
@@ -77,15 +77,11 @@
                     <a href="contact.php" class="nav-item nav-link">Contact Us</a>
                 </div>
                 <div class="d-none d-lg-flex ms-2">
-                    <a class="btn-sm-square bg-white rounded-circle ms-3" href="">
-                        <small class="fa fa-search text-body"></small>
-                    </a>
-                    <a class="btn-sm-square bg-white rounded-circle ms-3" href="">
+
+                    <a class="btn-sm-square bg-white rounded-circle ms-3" href="login.php">
                         <small class="fa fa-user text-body"></small>
                     </a>
-                    <a class="btn-sm-square bg-white rounded-circle ms-3" href="">
-                        <small class="fa fa-shopping-bag text-body"></small>
-                    </a>
+ 
                 </div>
             </div>
         </nav>
